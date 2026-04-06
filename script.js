9430dd9c1909fcf3dd6059962add24874a6096f2
Suraj Sharma <surajkrsharma207@gmail.com>
2026-04-06 15:02:24 +0530

    Add More Pages

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
 
+const quantityInputs = document.querySelectorAll("#cart input");
+const rows = document.querySelectorAll("#cart input");
+
 if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
@@ -13,3 +16,21 @@ if (close) {
         nav.classList.remove('active');
     });
 }
+
+quantityInputs.forEach((input) => {
+    input.addEventListener("change", () => {
+        const row = input.closest("tr");
+        const price = parseFloat(row.children[3].innerText.replace("₹", ""));
+        const quantity = input.value;
+
+        row.children[5].innerText = "₹" + (price * quantity).toFixed(2);
+    });
+});
+
+let total = 0;
+rows.forEach(row => {
+    const price = parseFloat(row.children[5].innerText.replace("₹",""));
+    total += price;
+});
+
+document.querySelector("#subtotal strong").innerText = "₹" + total;
